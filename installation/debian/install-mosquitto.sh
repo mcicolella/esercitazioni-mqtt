b244b06f9563dfdb77b5502299963962d5fa5fad
Mauro Cicolella <mcicolella@libero.it>
2020-07-17 18:01:14 +0200

    Create install-mosquitto.sh

---
+++ installation/debian/install-mosquitto.sh
@@ -0,0 +1,12 @@
+# Add repository
+sudo apt-add-repository ppa:mosquitto-dev/mosquitto-ppa
+
+# Update and install mosquitto
+sudo apt-get update
+sudo apt-get install mosquitto
+
+# Install Mosquitto Clients
+sudo apt-get install mosquitto-clients
+
+# Check service status
+sudo service mosquitto status
